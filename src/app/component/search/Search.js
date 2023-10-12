c13f67d1380ed522bfd6358926eef3b8248e3c8f
minfile <liangjiamin98@gmail.com>
2023-10-12 22:06:50 +0800

    merge main

--- src/app/component/search/Search.js
+++ src/app/component/search/Search.js
@@ -6,48 +6,7 @@ import InputBar from "../autosearch/autosearch";
 import Link from "next/link";
 function Search() {
   return (
-    <div>
-      <div className="flex flex-row mt-4">
-        <div className="mx-auto flex flex-row  max-sm:hidden">
-          <div className="">
-           {/* <InputBar /> */}
-            {/* <input className="outline-none	p-[10px] text-[16px] border-none	search-bar  bg-[#181818] max-4k:w-[1000px] max-2k:w-[800px] max-less2k:w-[700px] max-2xl:w-[600px] max-xl:w-[500px] max-lg:w-[360px] max-moremd:w-[200px]max-sm:hidden" /> */}
-            {/* <div className="search-bar "><SearchFilter /></div> */}
-          </div>
-          <Image
-            src="/icons8-search-50.png"
-            height={50}
-            width={50}
-            className="hoverEffectEl search-icon bg-[#323232]  max-sm:hidden"
-          />
-          <Image
-            src="/Mic.svg"
-            height={36}
-            width={36}
-            className="ml-6 max-md:hidden "
-          />
-        </div>
-
-        <div className="hoverEffectEl tools flex flex-row max-sm:ml-[86%] ">
-          <Image
-            src="/record.svg"
-            height={24}
-            width={24}
-            className="hoverEffectEl record mt-2.5 max-md:hidden"
-          />
-          <Image
-            src="/more.svg"
-            height={24}
-            width={24}
-            className="hoverEffectEl more mt-2.5 max-sm:hidden"
-          />
-          <Image
-            src="/alert.svg"
-            height={24}
-            width={24}
-            className="hoverEffectEl alert mt-2.5"
-          />
-<<<<<<< HEAD
+    < HEAD >
           <div className="search-group mt-2 flex flex-row max-sm:hidden">
             {/* search box */}
             <input
@@ -99,16 +58,8 @@ function Search() {
               />
             </Link>
           </div>
-=======
-          <Image
-            src="/Profile.svg"
-            height={24}
-            width={24}
-            className="hoverEffectEl Profile mt-2.5"
-          />
-        </div>
-      </div>
-    </div>
+     </HEAD>
+          
   );
 }
 export default Search;
